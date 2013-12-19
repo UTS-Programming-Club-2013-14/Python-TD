0fb44a8ad66d2b5d11d99c957d6781788f97092e
UTSProgrammingClub <utsprogramming@gmail.com>
2013-12-18 20:08:45 -0500

    Create outline for Tower.py

---
+++ Tower.py
@@ -0,0 +1,13 @@
+"""
+class Tower:
+@author: #PUT YOUR NAME HERE IF YOU WANT TO CLAIM THIS CLASS
+
+Parameters:
+range : int #How large its radius is in pixels. 
+attackSpeed : int #Delay in milliseconds between shots. Multiples of 50. 
+bulletType : Bullet #The type of bullet that this fires. 
+
+Methods:
+fire (self, target : Enemy) -> None:
+    "Fires a bullet at %target"
+"""
